9c754103ea0f69b8a906b51c68a070e195466504
Golu00001111 <129370862+Golu00001111@users.noreply.github.com>
2026-06-29 07:26:25 +0530

    Create MainActivity with onCreate method
    Added MainActivity class with onCreate method.

---
+++ app/src/main/java/com/example/offlineshope/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.offlineshope
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Yahan activity ka content set hoga
+    }
+}
+
