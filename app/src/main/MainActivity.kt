56a2eee288ac1598214fb8fa3b6936f04bbbe692
Golu00001111 <129370862+Golu00001111@users.noreply.github.com>
2026-06-29 07:40:18 +0530

    Create MainActivity with onCreate method
    Initial implementation of MainActivity with basic setup.

---
+++ app/src/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.offlineshope
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Yahan aap apna XML layout set kar sakte hain
+        // setContentView(R.layout.activity_main)
+    }
+}
+
